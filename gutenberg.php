1af8ebe303edc2fc96641a33d5f8c8059dd46317
Herb Miller <herb@bobbingwide.com>
2021-05-26 20:36:50 +0100

    gutenberg 10.7.1 - 2021-05-26

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
  * Requires at least: 5.6
  * Requires PHP: 5.6
- * Version: 10.7.0
+ * Version: 10.7.1
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '10.7.0' );
-define( 'GUTENBERG_GIT_COMMIT', 'cba6a64c9cfbe9efdb16eae4d45e1b152be39aa7' );
+define( 'GUTENBERG_VERSION', '10.7.1' );
+define( 'GUTENBERG_GIT_COMMIT', 'c2b9275b86eec1158bda9582161641a04314028e' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
