cf84cf0502679610c52e0510558468083d5a2218
Herb Miller <herb@bobbingwide.com>
2024-02-04 17:35:53 +0000

    gutenberg 17.4.0 - 2024-01-03

--- gutenberg.php
+++ gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.3
  * Requires PHP: 7.0
- * Version: 17.3.2
+ * Version: 17.4.0
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '17.3.2' );
-define( 'GUTENBERG_GIT_COMMIT', '71ff85b113634032a21b7f15f7b549db108fcb3a' );
+define( 'GUTENBERG_VERSION', '17.4.0' );
+define( 'GUTENBERG_GIT_COMMIT', 'd2550f889e26bff85f7fddac8f517460eef138c4' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
